ba26300e50096774d7be199f7a917b51e297e29b
Kris G. <Kris Grodahl>
2026-02-27 09:56:32 -0500

    Added modular filename to loader function and additional test text file

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from loader import load_users_from_file
 from services import get_active_admins
 from summary import generate_summary, print_summary
+import sys
 
+if len(sys.argv) > 1:
+    filename = sys.argv[1]
+else:
+    filename = "users.txt"
 
 def main():
-    users = load_users_from_file("users.txt")
+    users = load_users_from_file(filename)
     active_admins = get_active_admins(users)
     summary = generate_summary(users, active_admins)
     print_summary(summary)
-
 
 if __name__ == "__main__":
     main()
