04cf566d33b1af4f1ff1b8fab4cbe19c5746bb94
Kris G. <Kris Grodahl>
2026-03-02 20:26:21 -0500

    Added loader messages for error handling

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from loader import load_users_from_file
 from services import get_active_admins
 from summary import generate_summary, print_summary
 import sys
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s: %(message)s"
+)
+
+logger = logging.getLogger(__name__)
 
 if len(sys.argv) > 1:
     filename = sys.argv[1]
@@ -14,5 +22,11 @@ def main():
     summary = generate_summary(users, active_admins)
     print_summary(summary)
 
+    logger.info("Starting user pipeline")
+    logger.info("Loading users from file: " + filename)
+    logger.info("Total Users Loaded: " + str(len(users)))
+    logger.info(f"Total admins: {len(active_admins)}")
+
 if __name__ == "__main__":
     main()
+
